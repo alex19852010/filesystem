c41681ad5268516d995fdc5f1d8a8027ac623e00
Alex Bogushenko <bogushenkoas@ukr.net>
2024-02-12 17:32:07 +0300

    1 commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <unordered_set>
+#include <cstdlib>
+#include <filesystem>
+
+using namespace std;
+
+// namespace fs = filesystem;
+
+auto recursiveGetFileNamesByExtension = [](const filesystem::path& path, const string& extension) {
+    vector<string> fileNames;
+    
+    for (const auto& p : filesystem::recursive_directory_iterator(path)) {
+        if (p.is_regular_file() && p.path().extension() == extension) {
+            fileNames.push_back(p.path().filename().string());
+        }
+    }
+    
+    return fileNames;
+};
+
+int main()
+{
+
+
+string path = "D:\\dir1";
+string extension = ".txt";
+    
+vector<string> fileNames = recursiveGetFileNamesByExtension(path, extension);
+    
+for (const auto& fileName : fileNames) {
+        cout << fileName << endl;
+    }
+
+
+return 0;
+
+}
